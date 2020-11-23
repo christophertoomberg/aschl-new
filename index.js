72a9f0cd4e186d7cf3d47092305de11ad6c7a412
Christopher Gregor Toomberg <ctoomberg@gmail.com>
2020-11-23 16:59:34 +0100

    more progress

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let phrases = ['IT for a better environment','Recycling management and compliance', 'Organic materials processing', 'Compost plant management and reporting', 'Agricultural materials recycling'];
-document.getElementById('typing').textContent = '|';
+document.getElementById('typing').textContent = '';
 
 let letterIndex = 0;
 let phraseIndex = 0;
@@ -8,7 +8,7 @@ async function type() {
     //Writing a phrase.
     if (letterIndex <= phrases[phraseIndex].length && phraseIndex <= phrases.length) {
         document.getElementById('typing').textContent = '';
-        document.getElementById('typing').textContent += phrases[phraseIndex].substring(0, letterIndex + 1) + '|';
+        document.getElementById('typing').textContent += phrases[phraseIndex].substring(0, letterIndex + 1);
         letterIndex++;
 
         // If we have written all of the phrases. Start again.
