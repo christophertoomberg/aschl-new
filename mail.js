ca5fafdab42e57e4684ec633d48b18482ca15f78
Christopher Gregor Toomberg <ctoomberg@gmail.com>
2020-12-02 23:58:41 +0100

    form works

--- mail.js
+++ mail.js
@@ -1,21 +1,40 @@
 async function sendMail() {
-    const fullName = document.getElementById('full-name').value;
-    const clientEmail = document.getElementById('client-email').value;
-    const phone = document.getElementById('phone').value;
-    const message = document.getElementById('message').value;
+    const fullName = document.getElementById('full-name');
+    const clientEmail = document.getElementById('client-email');
+    const phone = document.getElementById('phone');
+    const message = document.getElementById('message');
 
-    const response = await fetch('https://aschl-mail.herokuapp.com/send', {
-        method: 'POST',
-        mode: 'cors',
-        headers: {
-            'Access-Control-Allow-Origin' : '*',
-            'Content-Type' : 'application/json',
-            'Accept' : '*/*',
-            'Accept-Encoding' : 'gzip, deflate, br',
-            'Connection' : 'keep-alive'
+    let status = document.getElementById('status');
 
-        },
-        body: {'fullName': fullName, 'clientEmail': clientEmail, 'phone': phone, 'message': message}
-    });
-    console.log(response);
+    try {
+        const response = await fetch('https://aschl-mail.herokuapp.com/send', {
+            method: 'POST',
+            mode: 'cors',
+            headers: {
+                'Access-Control-Allow-Origin' : '*',
+                'Content-Type' : 'application/json'
+            },
+            body: JSON.stringify({fullName: fullName.value.trim(), clientEmail: clientEmail.value.trim(), phone: phone.value.trim(), message: message.value.trim()})
+        });
+        if (response.status === 200) {
+            fullName.value = '';
+            clientEmail.value = '';
+            phone.value = '';
+            message.value = '';
+            status.classList.remove('error');
+            status.classList.add('success');
+            status.textContent = 'Thank you for contacting us!';
+        } else if (response.status == 400) {
+            const data = await response.json();
+            console.log(data);
+            status.classList.add('error');
+            status.innerHTML = '';
+            data.errors.forEach(errorMessage => {
+                const eMessage = errorMessage.msg;
+                status.innerHTML += eMessage + '<br>'
+            });
+        }
+    } catch (error) {
+        console.log(error);
+    }
 }
